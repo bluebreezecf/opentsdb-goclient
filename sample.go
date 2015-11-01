8763f77b740fdd3515c2520932d4b459e07a3355
bluebreezecf <bluebreezecf@gmail.com>
2015-11-01 22:22:27 +0000

    Chmod to 644

--- sample.go
+++ sample.go
@@ -27,7 +27,7 @@ import (
 )
 
 func main() {
-	tsdbClient, err := client.NewClient("127.0.0.1:4242")
+	tsdbClient, err := client.NewClient("9.91.34.95:4242")
 	if err != nil {
 		fmt.Printf("%v\n", err)
 		return
