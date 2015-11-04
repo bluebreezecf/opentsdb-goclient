f099b32eb47f9d95a40805db10570b144c84e786
bluebreezecf <bluebreezecf@gmail.com>
2015-11-04 00:03:21 +0000

    Refresh comments of client.Put()

--- client/client.go
+++ client/client.go
@@ -87,8 +87,8 @@ type Client interface {
 	// This endpoint allows for storing data in OpenTSDB over HTTP as an alternative to the Telnet interface.
 	//
 	// datas is a slice of DataPoint holding at least one instance.
-	// queryParam can only be common/opentsdbclient/client.PutRespWithSummary,
-	// common/opentsdbclient/client.PutRespWithDetails or the empty string "";
+	// queryParam can only be github.com/bluebreezecf/opentsdb-goclient/client.PutRespWithSummary,
+	// github.com/bluebreezecf/opentsdb-goclient/client.PutRespWithDetails or the empty string "";
 	// It means get put summary response info by using PutRespWithSummary, and
 	// with PutRespWithDetails means get put detailed response.
 	//
